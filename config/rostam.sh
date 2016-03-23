b01f60837788f3cae7aad3eac5477863fa39ece6
Headnode local administrator <akheir1@lsu.edu>
2016-03-23 09:56:44 -0500

    moving rostam.sh to config folder

---
+++ config/rostam.sh
@@ -0,0 +1,24 @@
+# Copyright (c) 2015-2016 Alireza Kheirkhahan
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+# /etc/profile.d/rostam.sh
+
+# System wide environment for rostam cluster, any customization goes here:
+
+# Modules generate by easybuild
+MODULEPATH=/opt/eb/modules/all:$MODULEPATH
+
+# Modules for Intel compilers
+MODULEPATH=/opt/modules:$MODULEPATH
+
+
+# Generating key pair to enable access through the cluster
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+    echo "Configuring ssh for cluster access"
+    install -d -m 700 $HOME/.ssh
+    ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -N '' > /dev/null 2>&1
+    cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+    chmod 0600 $HOME/.ssh/authorized_keys
+fi
