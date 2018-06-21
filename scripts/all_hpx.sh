57b1620778cd8da3084c37936c9daa0554ef9a7c
Alireza Kheirkhahan <kheirkhahan@gmail.com>
2018-06-21 10:48:13 -0500

    upgrading hpx build to gcc 8.1, clang 6.0 and boost 1.67

--- scripts/all_hpx.sh
+++ scripts/all_hpx.sh
@@ -7,14 +7,14 @@
 
 BUILD_SCRIPT=`dirname $0`/hpx_build.sh
 
-# Build with gcc 7.3.0 and boost 1.66.0 in debug
-$BUILD_SCRIPT -c gcc -v 7.3.0 -b 1.66.0
+# Build with gcc 8.1.0 and boost 1.67.0 in debug
+$BUILD_SCRIPT -c gcc -v 8.1.0 -b 1.67.0
 
-# Build with gcc 7.3.0 and boost 1.66.0 in release mode and no download
-$BUILD_SCRIPT -n -c gcc -v 7.3.0 -b 1.66.0 -o release
+# Build with gcc 8.1.0 and boost 1.67.0 in release mode and no download
+$BUILD_SCRIPT -n -c gcc -v 8.1.0 -b 1.67.0 -o release
 
-# Build with clang 6.0.0 and boost 1.66.0 in debug mode and no download
-$BUILD_SCRIPT -n -c clang -v 6.0.0 -b 1.66.0 -f '-DCMAKE_CXX_FLAGS=-stdlib=libc++'
+# Build with clang 6.0.0 and boost 1.67.0 in debug mode and no download
+$BUILD_SCRIPT -n -c clang -v 6.0.0 -b 1.67.0 -f '-DCMAKE_CXX_FLAGS=-stdlib=libc++'
 
-# Build with clang 6.0.0 and boost 1.66.0 in debug mode and no download
-$BUILD_SCRIPT -n -c clang -v 6.0.0 -o release -b 1.66.0 -f '-DCMAKE_CXX_FLAGS=-stdlib=libc++'
+# Build with clang 6.0.0 and boost 1.67.0 in debug mode and no download
+$BUILD_SCRIPT -n -c clang -v 6.0.0 -o release -b 1.67.0 -f '-DCMAKE_CXX_FLAGS=-stdlib=libc++'
